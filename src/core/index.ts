87e7324234b3fe22ef488f8943271b6f3137f67a
so1ve <i@mk1.io>
2023-05-15 21:10:04 +0800

    fix: make sure the plugin only execute on tsx files

--- src/core/index.ts
+++ src/core/index.ts
@@ -6,7 +6,11 @@ import { transform } from "./transform";
 export default createUnplugin<Options | undefined>((_options) => ({
   name: "unplugin-vue-tsx-auto-props",
   enforce: "pre",
-  transform(code) {
+  transform(code, id) {
+    if (!id.endsWith(".tsx")) {
+      return code;
+    }
+
     return transform(code);
   },
 }));
