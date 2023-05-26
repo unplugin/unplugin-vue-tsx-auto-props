d1007252a9eee807929d68f248f33771322e7ed2
so1ve <nn_201312@163.com>
2023-05-26 20:25:38 +0800

    chore: update deps

--- src/astro.ts
+++ src/astro.ts
@@ -2,14 +2,12 @@ import type { Options } from "./types";
 
 import unplugin from ".";
 
-export default function (options: Options) {
-  return {
-    name: "unplugin-vue-tsx-auto-props",
-    hooks: {
-      "astro:config:setup": async (astro: any) => {
-        astro.config.vite.plugins ||= [];
-        astro.config.vite.plugins.push(unplugin.vite(options));
-      },
+export default (options: Options) => ({
+  name: "unplugin-vue-tsx-auto-props",
+  hooks: {
+    "astro:config:setup": async (astro: any) => {
+      astro.config.vite.plugins ||= [];
+      astro.config.vite.plugins.push(unplugin.vite(options));
     },
-  };
-}
+  },
+});
